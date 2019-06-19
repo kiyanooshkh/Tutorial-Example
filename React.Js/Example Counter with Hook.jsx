12a01a8b6928d9915de7a7bb5db598c04d523fa1
Kiyanoosh Kh <khosravi.kiyanoosh@gmail.com>
2019-06-19 13:33:04 +0930

    Create Example Counter with Hook.jsx

---
+++ React.Js/Example Counter with Hook.jsx
@@ -0,0 +1,15 @@
+function logRandom(){
+  console.log(Math.random());
+}
+
+
+function Button(){
+  const [counter, setCounter] = useState(0);
+  
+  return <button onClick={()=> setCounter(counter+1)}>{counter}</button>
+}
+
+ReactDOM.render(
+  <Button/>,
+  document.getElementById('mountNode'),
+);
